e4500d6f96d310291ce612d9d577d637a7859bb5
Andrew Lauck <andrewlauck@andrews-air.wifi.uc.edu>
2022-12-01 13:25:20 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int n = 0;
+        while ( n <= 18)
+        {
+            System.out.println(n);
+            n=n+3;
+        }
+
+    }
+}
